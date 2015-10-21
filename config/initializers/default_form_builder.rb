d7d9c81af41205f0b5ff80bd73112b311dfc0e08
Eloy Espinaco <eloyesp@gmail.com>
2015-10-21 13:25:20 -0300

    Default form builder is in the Initializer.
    It fixes all the autoloading issues.

--- config/initializers/default_form_builder.rb
+++ config/initializers/default_form_builder.rb
@@ -1,1 +1,17 @@
-ActionView::Base.default_form_builder = CustomFormBuilder
+class DefaultFormBuilder < ActionView::Helpers::FormBuilder
+  def experience_level_select attribute = :level
+    select attribute, ExperienceLevel.options,
+      selected: object.public_send(attribute).to_i
+  end
+
+  def skill_select attribute = :skill_id
+    collection_select attribute, Skill.all, :id, :name, prompt: true
+  end
+
+  def seniority_select attribute = :seniority
+    select attribute, Seniority.options,
+      selected: object.public_send(attribute).to_i
+  end
+end
+
+ActionView::Base.default_form_builder = DefaultFormBuilder
