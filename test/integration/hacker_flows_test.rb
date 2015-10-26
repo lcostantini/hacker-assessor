4173794dc195881bf7ae487b50aef218c19abfa8
lcostantini <costantinileandro1@gmail.com>
2015-10-26 14:42:38 -0300

    Change dashboard boxes for table.

--- test/integration/hacker_flows_test.rb
+++ test/integration/hacker_flows_test.rb
@@ -4,17 +4,16 @@ class HackerFlowsTest < ActionDispatch::IntegrationTest
 
   test "jorge claim that he played with tdd" do
     login :jorge
-    assert has_content? "missing skills"
 
-    within 'li' do
+    within 'tbody' do
       assert has_content? 'tdd'
-      assert has_content? 'Required level: played'
+      assert has_content? 'played'
       click_on 'claim'
     end
 
     assert has_content?('Now you know tdd'), 'display a flash message'
 
-    within 'li' do
+    within 'tbody' do
       refute has_content? 'tdd'
     end
   end
